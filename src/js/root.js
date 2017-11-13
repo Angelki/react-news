5320afbf46a153ffa04ee9c80ca7874078104755
angelks <angelks4@live.com>
2017-11-13 16:39:47 +0800

    antd引入

--- src/js/root.js
+++ src/js/root.js
@@ -4,14 +4,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
+import  {Button} from 'antd';
+import  'antd/dist/antd.css';
 import registerServiceWorker from './registerServiceWorker';
 
 export  default  class Root extends React.Component {
     render() {
         return (
-            <div>init</div>
+            <div>
+                <Button type="primary">Primary</Button>
+                <Button>Default</Button>
+                <Button type="dashed">Dashed</Button>
+                <Button type="danger">Danger</Button>
+            </div>
         );
     };
 }
-ReactDOM.render(<Root />,document.getElementById('example'));
+ReactDOM.render(<Root />,document.getElementById('mainContainer'));
 registerServiceWorker();
