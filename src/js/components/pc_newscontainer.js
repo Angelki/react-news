1a32d04fb0791e6213505eeee02da2825f2ed313
angelks <angelks4@live.com>
2017-11-21 22:13:15 +0800

    Link版本有改动

--- src/js/components/pc_newscontainer.js
+++ src/js/components/pc_newscontainer.js
@@ -4,6 +4,7 @@
 import React from 'react';
 import {Row, Col} from 'antd';
 import {Tabs, Carousel} from 'antd';
+import PCNewsBlock from './pc_news_block';
 const TabPane = Tabs.TabPane;
 
 export default class PCNewsContainer extends React.Component {
@@ -23,6 +24,11 @@ export default class PCNewsContainer extends React.Component {
                                 </Carousel>
                             </div>
                         </div>
+                        <Tabs className="tabs_news">
+                            <TabPane tab="新闻" key="1">
+                                <PCNewsBlock count={20} type="top" width="100%" bordered="false" />
+                            </TabPane>
+                        </Tabs>
                     </Col>
                     <Col span={2}></Col>
                 </Row>
