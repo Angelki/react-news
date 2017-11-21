705e224abc0c008c6b738f911da6f8602d017210
angelks <angelks4@live.com>
2017-11-21 11:03:40 +0800

    mobile导航栏

--- src/js/components/mobile_index.js
+++ src/js/components/mobile_index.js
@@ -4,17 +4,34 @@
 import React from 'react';
 import MobileHeader from './mobile_header';
 import MobileFooter from './mobile_footer';
-import {Row, Col} from 'antd';
+import {Tabs} from 'antd';
 import '../../css/mobile.css';
 
+const TabPane = Tabs.TabPane;
 
-export default class MobileIndex extends React.Component {
+
+class MobileIndex extends React.Component {
     render() {
         return (
             <div>
                 <MobileHeader></MobileHeader>
+                <Tabs>
+                    <TabPane tab="头条" key="0">
+                    </TabPane>
+                    <TabPane tab="国内" key="1">
+                    </TabPane>
+                    <TabPane tab="国际" key="2">
+                    </TabPane>
+                    <TabPane tab="娱乐" key="3">
+                    </TabPane>
+                    <TabPane tab="体育" key="4">
+                    </TabPane>
+                </Tabs>
                 <MobileFooter></MobileFooter>
             </div>
         );
     };
 }
+
+export default MobileIndex;
+// export default MobileIndex = Form.create({})(MobileIndex);
