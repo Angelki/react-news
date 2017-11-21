2dc398f38e3d5d5afdfecbfce435396a84ac563b
angelks <angelks4@live.com>
2017-11-21 22:14:17 +0800

    Link暂时删除

--- src/js/components/pc_news_block.js
+++ src/js/components/pc_news_block.js
@@ -3,8 +3,8 @@
  */
 import React from 'react';
 import {Card} from 'antd';
-import {Route, Router, browserHistory} from 'react-router';
-import {Link} from 'react-router-dom';
+// import {Route, Router, browserHistory} from 'react-router';
+// import {Link} from 'react-router-dom';
 
 export default class PCNewsBlock extends React.Component {
     constructor() {
@@ -29,9 +29,9 @@ export default class PCNewsBlock extends React.Component {
             ?
             news.map((newsItem,index) => (
                 <li key={index}>
-                    <Link to={`details/${newsItem.uniquekey}`} target={"_blank"}>
+                    {/*<Link to={`details/${newsItem.uniquekey}`} target={"_blank"}>*/}
                         {newsItem.title}
-                    </Link>
+                    {/*</Link>*/}
                 </li>
             ))
             :
