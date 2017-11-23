9fa1000bbcb859c3406640f4d9d5d5adb65d1704
angelks <angelks4@live.com>
2017-11-23 19:43:27 +0800

    移动端轮播图

--- src/js/components/mobile_index.js
+++ src/js/components/mobile_index.js
@@ -4,7 +4,7 @@
 import React from 'react';
 import MobileHeader from './mobile_header';
 import MobileFooter from './mobile_footer';
-import {Tabs} from 'antd';
+import {Tabs, Carousel} from 'antd';
 // import '../../css/mobile.css';
 import MobileList from './mobile_list';
 const TabPane = Tabs.TabPane;
@@ -17,10 +17,19 @@ class MobileIndex extends React.Component {
                 <MobileHeader></MobileHeader>
                 <Tabs>
                     <TabPane tab="头条" key="0">
+                        <div className="carousel">
+                            <Carousel  autoplay>
+                                <div><img src="./1.png" alt=""/></div>
+                                <div><img src="./1.png" alt=""/></div>
+                                <div><img src="./1.png" alt=""/></div>
+                                <div><img src="./1.png" alt=""/></div>
+                            </Carousel>
+                        </div>
                         <MobileList count={20} type="top" />
                     </TabPane>
 
                     <TabPane tab="国内" key="1">
+                        <MobileList count={20} type="guonei" />
                     </TabPane>
                     <TabPane tab="国际" key="2">
                     </TabPane>
